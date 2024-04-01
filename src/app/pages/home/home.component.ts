fe48cc0f65c394a125edef7d178f70da30e53bfe
Yarinjo <gladysyora@gmail.com>
2024-04-01 17:21:23 +0300

    added homework for lesson 21

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  public coffeeData = [
+    {
+      title: 'Cappucino',
+      id: 1,
+    },
+    {
+      title: 'Latte',
+      id: 2,
+    },
+  ]
+
+}
